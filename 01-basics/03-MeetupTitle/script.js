819b12bc1262e65a99eb7bec031a898e0c7c45b7
Rustam Kalimulin <r.kalimulin@notamedia.ru>
2021-04-28 20:38:24 +0400

    add solution for MeetupTitle

--- 01-basics/03-MeetupTitle/script.js
+++ 01-basics/03-MeetupTitle/script.js
@@ -1,3 +1,43 @@
-// import Vue from './vendor/vue.esm.browser.js';
+import Vue from './vendor/vue.esm.browser.js';
 
-// new Vue();
+new Vue({
+  el: '#app',
+
+  data() {
+    return {
+      meetups: [],
+      activeMeetupId: '',
+      activeMeetup: {},
+      apiUrl: 'https://course-vue.javascript.ru/api',
+      loading: false,
+    }
+  },
+
+  methods: {
+    getMeetup(id) {
+      this.loading = true;
+      fetch(`${this.apiUrl}/meetups/${id}`)
+        .then((response) => {
+          return response.json();
+        })
+        .then((data) => {
+          this.loading = false;
+          this.activeMeetup = data;
+        })
+        .catch((error) => {
+          this.loading = false;
+          console.log(error);
+        });
+    }
+  },
+
+  created() {
+    this.getMeetupsList();
+  },
+
+  watch: {
+    activeMeetupId: function (id) {
+      this.getMeetup(id);
+    }
+  }
+});
